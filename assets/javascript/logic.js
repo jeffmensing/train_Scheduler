b5724a55ff890fc01afe0f9e6b902bc273b87aa6
Jeff Mensing <Jeff Mensing>
2017-03-11 21:56:11 -0600

    updating html and js files

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -13,24 +13,38 @@ var destination = "";
 var firstTrainTime = "";
 var frequency = "";
 var currentTime = "";
-
+var convertFirstTrainTime = "";
+var difference = "";
+var remainder = "";
+var minTillNextTrain = "";
+var nextTrain = "";
 
 $(document).ready(function() {
 
     $("#add-train").on("click", function() {
+        event.preventDefault();
         name = $("#name-input").val().trim();
         destination = $("#destination-input").val().trim();
         firstTrainTime = $("#first-train-time-input").val().trim();
         frequency = $("#frequency-input").val().trim();
         console.log(name);
         console.log(destination);
-
+        convertFirstTrainTime = moment(firstTrainTime, "hh:mm").subtract(1, "years");
+        currentTime = moment();
+        difference = moment().diff(moment(convertFirstTrainTime, "minutes"));
+        remainder = difference % frequency;
+        minTillNextTrain = frequency - remainder;
+        nextTrain = moment().add(minTillNextTrain, "minutes");
+        console.log(nextTrain);
+        nextTrain = moment(nextTrain).format("hh:mm");
+        console.log(nextTrain);
         //for testing firebase connection
-        firebase.database().ref().set({
+        firebase.database().ref().push({
             name: name,
             destination: destination,
             firstTrainTime: firstTrainTime,
-            frequency: frequency
+            frequency: frequency,
+            nextTrain: nextTrain
         })
 
 
@@ -45,7 +59,23 @@ $(document).ready(function() {
         //push data to firebase
 
         //display information to page
-    })
+    });
+
+    firebase.database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+        console.log(childSnapshot.val());
+        console.log(childSnapshot.val().destination);
+        console.log(childSnapshot.val().frequency)
+
+
+
+        $(".train-schedule").append("<tr class='table-row' id=" + "'" + childSnapshot.key() + "'" + ">" + "<td class ='col-xs-3'>" + childSnapshot.val().name + "</td>" +
+            "<td class ='col-xs-2'>" + childSnapshot.val().destination + "</td>" +
+            "<td class ='col-xs-2'>" + childSnapshot.val().frequency + "</td>" +
+            "<td class ='col-xs-2'>" + childSnapshot.val().nextTrain + "</td>" +
+            "<td class ='col-xs-2'>" + childSnapshot.val().minTillNextTrain + "</td>");
+
+    });
+
 
 
 
